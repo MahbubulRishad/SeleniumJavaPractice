0865b684fed73766fa37f3c0930abbfabcd59c29
MahbubulRishad <mahbubulrishad@gmail.com>
2023-07-24 01:31:56 +0600

    Selenium Practice

---
+++ src/test/java/SeleniumJavaPractice.java
@@ -0,0 +1,15 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SeleniumJavaPractice {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.firefoxdriver().setup();
+        WebDriver driver = new FirefoxDriver();
+
+        driver.get("https://demoqa.com/");
+        Thread.sleep(4000);
+
+        driver.quit();
+    }
+}
